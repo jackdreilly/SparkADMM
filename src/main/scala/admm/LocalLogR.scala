fa3df453599f969e1b091085045b5fb96e3a7a3c
Johann Grauzam <johann.grauzam@gmail.com>
2012-04-07 00:56:57 +0200

    localLogR fixed

--- src/main/scala/admm/LocalLogR.scala
+++ src/main/scala/admm/LocalLogR.scala
@@ -15,15 +15,17 @@ import cern.colt.matrix.tdouble.{DoubleFactory1D, DoubleMatrix1D}
 
 object LocalLogR {
   type DataPair = (SparseDoubleMatrix2D, SparseDoubleMatrix1D)
-  val ITERATIONS = 5
+  val ITERATIONS = 300
 
   def solve(sparseData: SparseDoubleMatrix2D, sparseOutput: SparseDoubleMatrix1D): DoubleMatrix1D ={
     val nSamples = sparseData.rows()
     val nFeatures = sparseData.columns()
     // Initialize w to a random value
-    val w = DoubleFactory1D.sparse.make(nFeatures,0.0)
+    val w = DoubleFactory1D.sparse.random(nFeatures)
+    w.assign(DoubleFunctions.mult(2.0))
+      .assign(DoubleFunctions.minus(1.0))
     for (i <- 1 to ITERATIONS) {
-      println("On iteration " + i)
+      // println("On iteration " + i)
       val gradient = DoubleFactory1D.sparse.make(nFeatures,0.0)
       for (j <- 0 until nSamples) {
         val b = sparseOutput.getQuick(j)
@@ -33,30 +35,50 @@ object LocalLogR {
         gradient.assign(p, DoubleFunctions.multSecond(scale))
       }
       w.assign(gradient, DoubleFunctions.minus)
-      println("card")
-      println(w.cardinality())
+     // println("card")
+     // println(w.cardinality())
     }
     w
   }
 
   def learnAndCheck(trainSet: DataPair, testSet: DataPair) {
     val wTrain = solve(trainSet._1, trainSet._2)
-    val ATest = testSet._1
-    val bTest = testSet._2
-    val bEst = ATest.zMult(wTrain,null)
-    println(bEst.cardinality())
+    println("wTrain")
+    println(wTrain)
+    val ATest = trainSet._1
+    println("Atest card")
+    println(ATest.cardinality())
+    // println("ATest")
+    // println(ATest.viewRow(4))
+    val bTest = trainSet._2
+    // println("bTest")
+    // println(bTest)
+    val bEst = ATest.zMult(wTrain,null,1.0,1.0,false)
+    //println("bEst")
+    //println(bEst)
     bEst.assign(DoubleFunctions.sign)
+    //println("bEst sign")
+    //println(bEst)
+    // println("bTest")
+    // println(bTest)
     bTest.assign(DoubleFunctions.mult(2.0))
          .assign(DoubleFunctions.minus(1.0))
          .assign(bEst,DoubleFunctions.minus)
+    // println("bTest - bEst")
+    // println(bTest)
+    println("result")
     println(1.0*bTest.cardinality()/bTest.size())
   }
 
   def main(args: Array[String]) {
-    val nSlices = 1
-    val nDocs = args(0).toInt
-    val nFeatures = args(1).toInt
+    // val nSlices = 1
+    // val nDocs = args(0).toInt
+    // val nFeatures = args(1).toInt
+    val nDocs = 600
+    val nFeatures = 600
     val A = RCV1Data.rcv1IDF(nDocs,2,nFeatures)
+   // println("A")
+   // println(A)
     val b = RCV1Data.labels(0,nDocs,2)
     val zippers = A.zip(b)
     learnAndCheck(zippers(0),zippers(1))
