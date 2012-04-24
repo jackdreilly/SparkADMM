2e9138b482e18dadf5757b2385c072705b8df69e
Jack Reilly <jackdreilly@gmail.com>
2012-04-23 17:42:18 -0700

    even better test

--- src/main/scala/tests/MutateParTest.scala
+++ src/main/scala/tests/MutateParTest.scala
@@ -1,7 +1,8 @@
 package tests
 
-import spark.SparkContext
-import tests.MutateParTest.Junk
+
+import spark.{RDD, SparkContext}
+import cern.colt.matrix.tdouble.{DoubleFactory1D, DoubleMatrix1D}
 
 /**
  * User: jdr
@@ -9,20 +10,60 @@ import tests.MutateParTest.Junk
  * Time: 5:14 PM
  */
 
-object MutateParTest extends App {
+object MutateParTest {
+  def simpleTest() {
+    // set up stuff
+    // create spark context
+    // create mutable class
+    val sc = new SparkContext("local", "jack")
+    class Junk extends Serializable {
+      var x = 1
+    }
 
-  val sc = new SparkContext("local", "jack")
+    // general test
+    def testMutation(rdd: RDD[Junk], testName: String) {
+      println("testing " +  testName + "\n##########\n########")
+      println("before mutate\n\n\n")
+      rdd.foreach(item => println(item.x))
+      rdd.foreach(item => item.x = 2)
+      println("after mutate\n\n\n")
+      rdd.foreach(item => println(item.x))
+    }
+    // test without caching
+    // this doesn't work because local variables are serialized and then dumped after each mapping
+    def testWithoutCache {
+      val rdd = sc.parallelize((1 to 3).map(i => new Junk))
+      testMutation(rdd, "No Cache")
+    }
+    // test with caching
+    // this works because a local variable is permanently stored in memory across mappings
+    // therefore when the print loop occurs, the reference to the mutated version is kept
+    def testWithCache {
+      val rdd = sc.parallelize((1 to 3).map(i => new Junk)).cache()
+      testMutation(rdd, "With Cache")
+    }
 
-  class Junk extends Serializable {
-    var x = 1
+    testWithoutCache
+    testWithCache
+
   }
 
-  val junks = (1 to 10).map(i => new Junk)
+  def vectorTest() {
+    val sc = new SparkContext("local", "jack")
+    class ExtraVector(vec1: DoubleMatrix1D) extends Serializable {
+      val vec2 = DoubleFactory1D.sparse.make(2)
+      def getVec1 = vec1
+    }
+    val cached = sc.parallelize(
+    (1 to 2).map(i => new ExtraVector(DoubleFactory1D.sparse.make(1)))
+    ).cache()
+    cached.foreach(vecs => println(vecs.vec2))
+    cached.foreach(vecs => vecs.vec2.setQuick(1,1))
+    cached.foreach(vecs => println(vecs.vec2))
+  }
+  def main(args: Array[String]) {
+    vectorTest()
+  }
 
-  val junksPar = sc.parallelize(junks).cache()
-
-  junksPar.foreach(junk => println(junk.x))
-  junksPar.foreach(junk => junk.x = 2)
-  junksPar.foreach(junk => println(junk.x))
 
 }
